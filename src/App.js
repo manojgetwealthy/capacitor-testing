1a446792f4ed57018c7fddd0c9353e1d79ffdb93
Brijesh Pandey <brijesh@wealthy.in>
2022-07-25 17:29:14 +0530

    Added sample capacitor camera api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
-function App() {
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonCard,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonCardContent,
+  IonItem,
+  IonIcon,
+  IonLabel,
+  IonButton,
+  IonImg
+} from "@ionic/react";
+import { pin, wifi, wine, warning, walk } from "ionicons/icons";
+
+// import {Plugins, CameraResultType} from '@capacitor/core';
+
+import { Camera, CameraResultType } from '@capacitor/camera';
+
+// const {Camera} = Plugins;
+
+
+
+const App = () => {
+  const [image, setImage] = useState();
+
+  const takePicture = async () => {
+    const image = Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Base64
+    });
+
+    let imageUrl = `data:image/jpeg:base64 ${(await image).base64String}`;
+
+    setImage(imageUrl);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>CardExamples</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonCard>
+          <IonCardHeader>
+            <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+            <IonCardTitle>Card Title</IonCardTitle>
+          </IonCardHeader>
+
+          <IonCardContent>
+            Keep close to Nature's heart... and break clear away, once in
+            awhile, and climb a mountain or spend a week in the woods. Wash your
+            spirit clean.
+          </IonCardContent>
+        </IonCard>
+
+        <IonCard>
+          <IonItem>
+            <IonIcon icon={pin} slot="start" />
+            <IonLabel>ion-item in a card, icon left, button right</IonLabel>
+            <IonButton fill="outline" slot="end">
+              View
+            </IonButton>
+          </IonItem>
+
+          <IonCardContent>
+            This is content, without any paragraph or header tags, within an
+            ion-cardContent element.
+          </IonCardContent>
+        </IonCard>
+
+        <IonCard>
+          <IonItem href="#" className="ion-activated">
+            <IonIcon icon={wifi} slot="start" />
+            <IonLabel>Card Link Item 1 activated</IonLabel>
+          </IonItem>
+
+          <IonItem href="#">
+            <IonIcon icon={wine} slot="start" />
+            <IonLabel>Card Link Item 2</IonLabel>
+          </IonItem>
+
+          <IonItem className="ion-activated">
+            <IonIcon icon={warning} slot="start" />
+            <IonLabel>Card Button Item 1 activated</IonLabel>
+          </IonItem>
+
+          <IonItem>
+            <IonIcon icon={walk} slot="start" />
+            <IonLabel>Card Button Item 2</IonLabel>
+          </IonItem>
+        </IonCard>
+
+        <IonItem>
+          <IonButton color="primary" onClick={() => takePicture()}>Take Photo</IonButton>
+          {image && (
+            <IonImg src={image} />
+          )}
+
+
+          </IonItem>
+
+      </IonContent>
+    </IonPage>
   );
-}
+};
 
 export default App;
